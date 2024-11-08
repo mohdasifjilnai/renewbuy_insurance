03fce3f75e9be592b4ce0f7da3a6e1e347c79cd2
dinesh kumar <dinesh.kumar@renewbuy.com>
2024-11-08 15:23:29 +0530

    FIX: fixed UI issue

--- src/app/insurance-landing-page/cashless-garages/cashless-garages.component.ts
+++ src/app/insurance-landing-page/cashless-garages/cashless-garages.component.ts
@@ -42,7 +42,9 @@ export class CashlessGaragesComponent {
   hospitalsList = [];
   insurar_data: any;
   cardList: any = [];
+  smallCardList:any = [];
   totalCardList: any = [];
+  isViewAll: boolean= true;
   constructor(
     private fb: FormBuilder,
     private apiService: ApiService,
@@ -121,9 +123,15 @@ export class CashlessGaragesComponent {
     this.apiService.getRequestedResponse(url, header).subscribe((response) => {
       this.totalCardList = response?.data;
       this.cardList = response?.data?.slice(0, 12);
+      this.smallCardList = response?.data?.slice(0,12);
     });
   }
   ViewAll() {
-    this.cardList = this.totalCardList;
+    this.isViewAll = !this.isViewAll;
+    if(this.isViewAll){
+      this.cardList = this.smallCardList;
+    }else{
+      this.cardList = this.totalCardList;
+    }
   }
 }
