d17bdbbdf7f35b28c5e5e35addb132ee5d71119c
Ramesh Singh <ramesh.singh@renewbuy.com>
2024-11-11 18:00:39 +0530

    change

--- server.ts
+++ server.ts
@@ -68,7 +68,7 @@ function run(): void {
     // const token = cookies['authToken']; // Replace 'authToken' with the actual token name
     const cookies = cookie.parse(req.headers.cookie || "");
     const token = `${cookies["access_token"]}`;
-    const tokenfron = "renewbuy";
+    const tokenfron = "renewbuy_insurance";
     const name = `${cookies["username"]}`;
 
     // Handle messages received from WebSocket clients
