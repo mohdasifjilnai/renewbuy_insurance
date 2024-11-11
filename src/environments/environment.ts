83276981abab60ce2046a8d38c52a804da6af19a
Ramesh Singh <ramesh.singh@renewbuy.com>
2024-11-11 17:50:14 +0530

    chages done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   strapiDomain: 'https://cms.rbstaging.in',
-  subDomain: 'https://react.renewbuyinsurance.in',
+  subDomain: 'react.renewbuyinsurance.in',
   unicornDomain: 'https://api-unicorn.rbstaging.in',
   dashboardDomain: 'https://myaccount.rbstaging.in',
   renewbuyInsuranceDomain: 'https://react.renewbuyinsurance.in',
