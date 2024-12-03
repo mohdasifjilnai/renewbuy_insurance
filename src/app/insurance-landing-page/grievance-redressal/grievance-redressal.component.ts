cbb099fb630b319ee58752c73fb7ef218f50ceec
asif <mohdasif.jilani@renewbuy.com>
2024-12-03 17:21:08 +0530

    FIX: add diler and mailer

--- src/app/insurance-landing-page/grievance-redressal/grievance-redressal.component.ts
+++ src/app/insurance-landing-page/grievance-redressal/grievance-redressal.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-grievance-redressal',
-  templateUrl: './grievance-redressal.component.html',
-  styleUrl: './grievance-redressal.component.scss'
+  selector: "app-grievance-redressal",
+  templateUrl: "./grievance-redressal.component.html",
+  styleUrl: "./grievance-redressal.component.scss",
 })
 export class GrievanceRedressalComponent {
-
+  IGMS() {
+    window.open(
+      "https://www.irdai.gov.in/ADMINCMS/cms/NormalData_Layout.aspx?page=PageNo226&mid=14.3",
+      "_blank"
+    );
+  }
 }
